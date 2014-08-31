00b28367e2665de4ade2b461734d286ee531f08d
Guilherme Bomfim <gbomfim@Guilhermes-MacBook-Pro.local>
2014-08-31 20:58:54 -0300

    subindo a tela index com para colocarmos no ar como teste de necessidade.
    Committer: Guilherme Bomfim <gbomfim@Guilhermes-MacBook-Pro.local>

--- app/myapp/public/javascripts/index.js
+++ app/myapp/public/javascripts/index.js
@@ -1,17 +1,40 @@
 
 function ready() {
-	var btn = document.querySelector("#btn");
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 
-    document.querySelector("#textBox").value = "antes";
+	var btn = document.querySelector("#btn");
+    var inputText = document.querySelector("#email").value = "";
 
 	btn.onclick = function() {
-		var inputText = document.querySelector("#inputText");
+		var inputText = document.querySelector("#email");
 
 		var text = inputText.value;
 
-		document.querySelector("#textBox").value = text;
+        if(validateEmail(text)){
+            $.ajax({
+                url: 'saveEmail',
+                type: 'POST',
+                data: {
+                    email: text
+                }
+            }).done(function(res) {
+                eventGA.track('email');
+
+                $(".sendEmailDiv").addClass("hide");
+                $(".emailSentDiv").removeClass("hide");
+            });
+        }
 	}
 }
+
+var eventGA = {
+    track: function(tag) {
+
+    }
+};
 
 window.addEventListener("load",ready,false);
 window.onload = ready;
--- app/myapp/routes/index.js
+++ app/myapp/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.post('/saveEmail', function(req, res) {
+    var email = req.body.email + "\n";
+
+    var fs = require('fs');
+    var error = false;
+    var stream = fs.appendFile("emails.txt",email,function(err) {
+        if (err) {
+            error = true;
+        }
+    },function() {
+        if(!error) {
+            res.json({ok:true});
+        }
+        else {
+            res.json(500,{ ok:false});
+        }
+        res.end();
+    });
+});
+
+
 module.exports = router;
